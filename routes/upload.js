e8fad6b0b3b8d188e814e2c4838dbbc9b6cd5eac
sidhart25 <navneetnishchal1420@gmail.com>
2024-12-30 18:27:59 +0530

    edit api is added

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createBlog, createReport } from "../controllers/upload.js";
 import { verifyUser } from "../utils/verifyTokens.js";
-import { cReport } from "../controllers/updatedReport.js";
+import { cReport, editReport } from "../controllers/updatedReport.js";
 
 const router = express.Router();
 
 router.post("/uploadreport", createReport);
 router.post("/uploadblog", createBlog);
 router.post("/ureport", cReport);
+router.put("/ureport", editReport);
 
 export default router;
